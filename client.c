8fa4f2f606fe6f580d522bfa1dd2d5b737c42be8
yiwen950725 <tiffany-gu@hotmail.com>
2021-02-22 16:46:10 -0800

    update

--- client.c
+++ client.c
@@ -1,171 +1,39 @@
-/*
- * Copyright 2021 D'Arcy Smith + the BCIT CST Datacommunications Option students.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+#include <dc/stdio.h>
+#include <dc/unistd.h>
+#include <dc/sys/socket.h>
+#include <dc/netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "fsm.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "shared.h"
+#include <netdb.h>
 
-static int connect_to_server(Environment *env);
-static int wait(Environment *env);
-static int write_to_server(Environment *env);
-static int close(Environment *env);
-static int end(Environment *env);
-_Noreturn static int error_exit(Environment *env);
-
-typedef enum
-{
-
-    GAME_START = FSM_APP_STATE_START, // 2
-    IDLE,       // 3
-    ERROR,      // 4
-    INPUT,      // 5
-    RESULT,     // 6
-    END,        // 7
-
-} States;
-
-typedef struct
-{
-    Environment common;
-    int c;
-} EchoEnvironment;
 
 int main(int argc, char *argv[])
 {
-    EchoEnvironment env;
-    StateTransition transitions[] =
-        {
-            {FSM_INIT, GAME_START, &connect_to_server},
-            {GAME_START, IDLE, &wait},
-            {GAME_START, INPUT, &write_to_server},
-            {INPUT, IDLE, &wait},
-            {INPUT, INPUT, &wait},
-            {IDLE, INPUT, &write_to_server},
-            {IDLE, RESULT, &close},
-            {RESULT, END, &end},
-            {FSM_IGNORE, FSM_IGNORE, NULL},
-        };
-    int code;
-    int start_state;
-    int end_state;
-    fprintf(stderr, "Press enter key \n");
-    start_state = FSM_INIT;
-    end_state = GAME_START;
-    code = fsm_run((Environment *)&env, &start_state, &end_state, transitions);
-
-    if (code != 0)
+    struct hostent *hostinfo;
+    struct sockaddr_in addr;
+    int fd;
+    ssize_t num_read;
+    char buf[BUF_SIZE];
+    
+    hostinfo = dc_gethostbyname("127.0.0.1");
+    fd = dc_socket(AF_INET, SOCK_STREAM, 0);
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr = *(struct in_addr *) hostinfo->h_addr;
+    dc_connect(fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+    
+    while((num_read = dc_read(STDIN_FILENO, buf, BUF_SIZE)) > 0)
     {
-        fprintf(stderr, "Cannot move from %d to %d\n", start_state, end_state);
-
-        return EXIT_FAILURE;
+        dc_write(fd, buf, num_read);
     }
-
-    fprintf(stderr, "Exiting state %d\n", start_state);
+    
+    dc_close(fd);
 
     return EXIT_SUCCESS;
-}
-
-static int connect_to_server(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-
-
-    fprintf(stderr, "You have entered the game!\n");
-    
-    return IDLE;
-}
-
-static int wait(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-    int position = 0;
-    char *buffer = malloc(sizeof(char)*1024);
-    int c;
-
-    echo_env = (EchoEnvironment *)env;
-    while (1)
-    {
-        fprintf(stderr, "Enter input!!\n");
-        c = getchar();
-        if(c == EOF || c == '\n'){
-            buffer[position] ='\0';
-            break;
-        }
-        position = position + 1;
-    }
-    echo_env->c = buffer[0];
-    if (echo_env->c == EOF)
-    {
-        if (ferror(stdin))
-        {
-            return ERROR;
-        }
-
-        return FSM_EXIT;
-    }
-
-    fprintf(stderr, "Transferring to input state!!\n");
-
-    return INPUT;
-}
-
-static int write_to_server(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-   
-    if (echo_env->c > 73 || echo_env->c < 65)
-    {
-        fprintf(stderr, "This is NOT a valid input! Try again!\n");
-        return INPUT;
-    }
-    fprintf(stderr, "Input valid!\n");
-
-    return IDLE;
-}
-
-static int close(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-   
-    
-    fprintf(stderr, "You win\n");
-
-    return RESULT;
-}
-
-static int end(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-    fprintf(stderr, "Bye bye!!\n");
-
-    return FSM_EXIT;
-}
-_Noreturn static int error_exit(Environment *env)
-{
-    perror("Error! Disconnected!");
-
-    exit(EXIT_FAILURE);
 }
--- server.c
+++ server.c
@@ -14,165 +14,267 @@
  * limitations under the License.
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "fsm.h"
+#include <string.h>
+#include <unistd.h>
+#include <dcfsm/fsm.h>
+#include <dc/stdio.h>
+#include <dc/unistd.h>
+#include <dc/sys/socket.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "shared.h"
 
-static int announceWinner(Environment *env);
-static int nextPlayer(Environment *env);
-static int check(Environment *env);
-static int waitForMove(Environment *env);
-static int read(Environment *env);
-static int write(Environment *env);
-static int connect(Environment *env);
-_Noreturn static int errorExit(Environment *env);
+#define BACKLOG 2
+
+static int init_server();
+static int read_input(Environment *env);
+static int write_code(Environment *env);
+static int prompt(Environment *env);
+static int read_error(Environment *env);
+_Noreturn static int write_error(Environment *env);
+
+int i = 0;
+int moves = 0;
+int client_socket[BACKLOG];
+int max_sd;
 
 typedef enum
 {
-
-    GAME_START = FSM_APP_STATE_START, // 2
-    IDLE,       // 3
-    ERROR,      // 4
-    VALIDATE,   // 5
-    RESULT,     // 6
-    CHECKSTATE,     // 7
+    PROMPT = FSM_APP_STATE_START,  // 2
+    INIT_SERVER,
+    AWAIT_CONNECTION,              // 3
+    READ,                          // 4
+    WRITE,                         // 5
+    WIN,                           // 6
+    LOSE,                          // 7
+    TIE,                           // 8
+    ERROR,                         // 9
 } States;
+
 
 typedef struct
 {
     Environment common;
     int c;
+    int code;
 } EchoEnvironment;
+
 
 int main(int argc, char *argv[])
 {
     EchoEnvironment env;
     StateTransition transitions[] =
-        {
-            {FSM_INIT, GAME_START, &connect},
-            {GAME_START, IDLE, &waitForMove},
-            {IDLE, VALIDATE, &read},
-            {VALIDATE, IDLE, &waitForMove},
-            
-            {VALIDATE, CHECKSTATE, &check},
-            {CHECKSTATE, RESULT, &announceWinner},
-            {CHECKSTATE, IDLE, &nextPlayer},
-            {VALIDATE, ERROR, &errorExit},
-    
-            {FSM_IGNORE, FSM_IGNORE, NULL},
-        };
+    {
+            { FSM_INIT,   INIT_SERVER,     &init_server},
+            { INIT_SERVER,   PROMPT,     &prompt},
+            { PROMPT,     READ,       &read_input   },
+            { READ,       ERROR,      &read_error   },
+            { READ,       WRITE,      &write_code   },
+            { READ,       TIE,        &prompt       },
+            { TIE,       FSM_EXIT,NULL      },
+            { READ,       FSM_EXIT,   NULL  },
+            { WRITE,      ERROR,      &write_error  },
+            { WRITE,      READ,       &read_input   },
+            {ERROR,       PROMPT,     &prompt       },
+            { FSM_IGNORE, FSM_IGNORE, NULL          },
+    };
     int code;
     int start_state;
     int end_state;
-    fprintf(stderr, "Press enter key \n");
-    start_state = FSM_INIT;
-    end_state = GAME_START;
+
+    start_state = FSM_INIT; // 0
+    end_state   = INIT_SERVER;
     code = fsm_run((Environment *)&env, &start_state, &end_state, transitions);
 
-    if (code != 0)
+    if(code != 0)
     {
         fprintf(stderr, "Cannot move from %d to %d\n", start_state, end_state);
 
         return EXIT_FAILURE;
     }
 
-    fprintf(stderr, "Exiting state %d\n", start_state);
+//    fprintf(stderr, "Exiting state %d\n", start_state);
 
     return EXIT_SUCCESS;
 }
 
-static int connect(Environment *env)
+static int init_server()
 {
-    EchoEnvironment *echo_env;
+    printf("Initing server...\n");
+    struct sockaddr_in addr;
+    int sfd;
 
-    echo_env = (EchoEnvironment *)env;
-
-
-    fprintf(stderr, "Two players have entered the game!\n");
-    return IDLE;
-}
-
-static int waitForMove(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-    int position = 0;
-
-    echo_env = (EchoEnvironment *)env;
-    while (position < 1)
+    //initialise all client_socket[] to 0 so not checked
+    for (int i = 0; i < BACKLOG; i++)
     {
-        fprintf(stderr, "Enter input!!\n");
-        echo_env->c = getchar();
-        position = position + 1;
+        client_socket[i] = 0;
     }
 
-    if (echo_env->c == EOF)
+    sfd = dc_socket(AF_INET, SOCK_STREAM, 0);
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    dc_bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+    dc_listen(sfd, BACKLOG);
+    printf("awaiting connection...\n");
+    for (int j = 0; j < BACKLOG; j++)
     {
-        if (ferror(stdin))
+        ssize_t num_read;
+        char buf[BUF_SIZE];
+
+        client_socket[j] = dc_accept(sfd, NULL, NULL);
+
+        while((num_read = dc_read(client_socket[j], buf, BUF_SIZE)) > 0)
+        {
+            dc_write(STDOUT_FILENO, buf, num_read);
+        }
+
+        dc_close(client_socket[j]);
+    }
+
+    // dc_close(sfd); <- never reached because for(;;) never ends.
+    return PROMPT;
+}
+
+static int read_input(Environment *env)
+{
+    EchoEnvironment *echo_env;
+
+    echo_env = (EchoEnvironment *)env;
+    echo_env->c = getchar();
+
+    if(echo_env->c == EOF)
+    {
+
+        if(ferror(stdin))
         {
             return ERROR;
         }
+        printf("EOF\n");
+        return FSM_EXIT;
+    } else if (echo_env->c == '\n') {
+        return WRITE;
+    }
 
+    switch (i) {
+        case 0:
+            if (echo_env->c == '1' || echo_env->c == '2') {
+                printf("validating player...\n");
+                echo_env->code = 200;
+            } else  {
+                i = 0;
+                echo_env->code = 400;
+                return ERROR;
+            }
+            break;
+        case 1:
+            if ((echo_env->c - '0') >= 0 && (echo_env->c - '0') <= 2 && echo_env->code == 200) {
+                printf("validating row...\n");
+                echo_env->code = 201;
+            } else {
+                i = 0;
+                echo_env->code = 401;
+                return ERROR;
+            }
+            break;
+        case 2:
+            if ((echo_env->c - '0') >= 0 && (echo_env->c - '0') <= 2 && echo_env->code == 201) {
+                printf("validating column...\n");
+                echo_env->code = 202;
+            } else {
+                echo_env->code = 402;
+                i = 0;
+                return ERROR;
+            }
+            break;
+        default:
+            i = 0;
+            return ERROR;
+    }
+
+    i++;
+    if (i == 3) {
+        i = 0;
+        moves++;
+    }
+    if (moves == 5) {
+        return TIE;
+    }
+    return WRITE;
+}
+
+static int write_code(Environment *env)
+{
+    EchoEnvironment *echo_env;
+    int              ret_val;
+
+    echo_env = (EchoEnvironment *)env;
+    ret_val = putchar(echo_env->c);
+
+    if(ret_val == EOF)
+    {
+        return ERROR;
+    }
+
+    return READ;
+}
+
+static int prompt(Environment *env)
+{
+    char* prompt = "Prompt: Place a move\n";
+    EchoEnvironment *echo_env;
+    int              ret_val;
+
+    echo_env = (EchoEnvironment *)env;
+    ret_val = echo_env->code;
+    if (moves == 5) {
+        printf("==========TIE==========\n");
         return FSM_EXIT;
     }
-
-    position = 0;
-    fprintf(stderr, "Time to validate the input!!\n");
-
-    return VALIDATE;
-}
-
-static int read(Environment *env)
-{
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-   
-    if (echo_env->c > 73 || echo_env->c < 65)
-    {
-        fprintf(stderr, "This is NOT a valid input! Try again!\n");
-        return IDLE;
+    switch (ret_val) {
+        case 400:
+            printf("Invalid player\n");
+            break;
+        case 401:
+            printf("Renter your move\n");
+            break;
+        case 402:
+            printf("Renter your move\n");
+            break;
+        default:
+            printf("%s", prompt);
+            break;
     }
-    fprintf(stderr, "Input valid!\n");
-
-    return CHECKSTATE;
+    echo_env->code = 0;
+    return READ;
 }
 
-static int nextPlayer(Environment *env){
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-    fprintf(stderr, "Next player's turn!\n");
-
-    return IDLE;
-
-}
-
-static int check(Environment *env)
+static int read_error(Environment *env)
 {
     EchoEnvironment *echo_env;
+    int              ret_val;
 
     echo_env = (EchoEnvironment *)env;
-    fprintf(stderr, "Time to announce the winner!\n");
-
-    return RESULT;
+    ret_val = echo_env->code;
+    if (ret_val == 401) {
+        printf("invalid row\n");
+        return PROMPT;
+    } else if (ret_val == 402) {
+        printf("invalid column\n");
+        return PROMPT;
+    } else {
+        printf("invalid moves\n");
+        return PROMPT;
+    }
 }
 
-
-static int announceWinner(Environment *env)
+_Noreturn static int write_error(Environment *env)
 {
-    EchoEnvironment *echo_env;
-
-    echo_env = (EchoEnvironment *)env;
-    fprintf(stderr, "Player 1 wins!!\n");
-
-    return FSM_EXIT;
-}
-
-_Noreturn static int errorExit(Environment *env)
-{
-    perror("Error! Disconnected!");
+    perror("putc");
 
     exit(EXIT_FAILURE);
 }
